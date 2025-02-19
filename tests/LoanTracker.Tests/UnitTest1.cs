9d0e65f0ff1161778448b39aa4e5282337ea55fe
pgpva <170962121+pgpva@users.noreply.github.com>
2025-02-19 13:58:11 -0600

    fix: create empty app

---
+++ tests/LoanTracker.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace LoanTracker.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
--- tests/LoanTracker.Tests/obj/Debug/net8.0/LoanTracker.Tests.AssemblyInfo.cs
+++ tests/LoanTracker.Tests/obj/Debug/net8.0/LoanTracker.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoanTracker.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bffdcfbfa3ac46a6e1f8599e098535fd4036391")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoanTracker.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoanTracker.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
