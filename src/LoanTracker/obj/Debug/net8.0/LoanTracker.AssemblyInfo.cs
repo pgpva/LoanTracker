3882740bc5a79d33f764033c1fb137e88c80da3f
pgpva <170962121+pgpva@users.noreply.github.com>
2025-03-05 06:12:03 -0600

    feat: Basic Authentication
    closes #8

--- src/LoanTracker/obj/Debug/net8.0/LoanTracker.AssemblyInfo.cs
+++ src/LoanTracker/obj/Debug/net8.0/LoanTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoanTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4542d0a48abe561d252736d78a3391796a0e7dae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3b79ae0ade69c33f078a271b77f0ab114fdd1f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoanTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoanTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
